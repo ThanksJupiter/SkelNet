fc6428cc43f32e64dbd3cd3c65f71cd66dc32199
ThanksJupiter <35657860+ThanksJupiter@users.noreply.github.com>
2020-01-21 09:45:41 +0100

    Missed SNAnimator.h in previous commit

--- SkelNet/SNAnimator.h
+++ SkelNet/SNAnimator.h
@@ -7,6 +7,14 @@ class SNSprite;
 struct SNAnimation;
 struct SDL_Texture;
 
+typedef enum
+{
+	ANIMS_IDLE = 0x00000001,
+	ANIMS_WALK = 0x00000002,
+	ANIMS_ATTACK = 0x00000004,
+	ANIMS_KNOCKBACK = 0x00000008
+} ANIM_State;
+
 class SNAnimator
 {
 public:
@@ -17,13 +25,19 @@ public:
 	double timer = 0.0f;
 	double nextFrameDelay = 0.25;
 
+	ANIM_State currentState;
+	bool movementLocked = false;
+	bool returnToPreviousAnimWhenDone = false;
+
 	float scale = 3.0;
 
 	bool isWalking = false;
 
 	SNAnimation* currentAnimation;
+	SNAnimation* previousAnimation;
 	int direction = 0;
 
 	void DrawAnimation(Vector2 position, bool flipped, float dt);
-	void SetCurrentAnimation(SNAnimation* inAnim, int frameCount);
+	void SetCurrentAnimation(SNAnimation* inAnim, bool oneShot = false);
+	bool IsCurrentAnimationDonePlaying();
 };
