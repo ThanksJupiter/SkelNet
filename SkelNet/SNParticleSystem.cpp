2766e9b9b868657a7005bd06bdfd7b5a3cd398df
KasperL <45844138+KasperLindm@users.noreply.github.com>
2020-01-23 14:28:05 +0100

    Added Particle effect functionality

---
+++ SkelNet/SNParticleSystem.cpp
@@ -0,0 +1,64 @@
+#include "SNParticleSystem.h"
+
+SNParticleSystem::SNParticleSystem()
+{
+
+}
+
+SNParticleEffect* SNParticleSystem::ReuseParticleEffect()
+{
+	for (int i = 0; i < particleEffects.size(); i++)
+	{
+		if (!&particleEffects[i])
+			continue;
+
+		if (!&particleEffects[i]->isUsed)
+		{
+			return particleEffects[i];
+		}
+		else
+		{
+			return new SNParticleEffect();
+		}
+	}
+	return new SNParticleEffect();
+}
+
+//call when you want to start the playback of one animation for a set duration at a location
+SNParticleEffect* SNParticleSystem::StartParticleEffect(Vector2 position, SNAnimation* animation, float duration)
+{
+	SNParticleEffect* newEffect = ReuseParticleEffect();
+	if (newEffect)
+	{
+		newEffect->currentPlayDuration = 0;
+		newEffect->playDuration = duration;
+		newEffect->position = position;
+		newEffect->particleAnimation = animation;
+
+		newEffect->particleAnimator = SNAnimator();
+
+		particleEffects.push_back(newEffect);
+		newEffect->particleAnimator.SetCurrentAnimation(newEffect->particleAnimation);
+		
+		return newEffect;
+	}
+}
+
+void SNParticleSystem::PlayParticleEffect(float dt)
+{
+	for (int i = 0; i < particleEffects.size(); i++)
+	{
+		if (!&particleEffects[i] || !&particleEffects[i]->particleAnimator)
+			continue;
+
+		particleEffects[i]->currentPlayDuration += dt;
+		if (particleEffects[i]->currentPlayDuration < particleEffects[i]->playDuration)
+		{
+			particleEffects[i]->particleAnimator.DrawAnimation(particleEffects[i]->position, false, dt);
+		}
+		else
+		{
+			particleEffects[i]->isUsed = false;
+		}
+	}
+}
