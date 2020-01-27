5d6a3039a6a00ca86b8bdc02d7ac0c58e7106649
KasperL <45844138+KasperLindm@users.noreply.github.com>
2020-01-27 11:49:10 +0100

    updated fall state

--- SkelNet/SNFSMFallState.cpp
+++ SkelNet/SNFSMFallState.cpp
@@ -6,30 +6,41 @@
 
 void SNFSMFallState::Enter(SNFSMData* fsmData)
 {
-	
+
 }
 
 void SNFSMFallState::Update(float dt, SNFSMData* fsmData)
 {
-	SNInput* input = fsmData->input;
 	SNAutonomousProxy* autoProxy = fsmData->autonomousProxy;
+	SNInput* input = fsmData->input;
 
+	autoProxy->SetDirection();
 	autoProxy->acceleration.y = autoProxy->gravity * autoProxy->gravityMult;
 
-	// movement time integration
 	autoProxy->previousPosition = autoProxy->position;
 
 	autoProxy->velocity += autoProxy->acceleration * dt;
 	autoProxy->position += autoProxy->velocity * dt;
 
-	if (autoProxy->velocity.y > 0 && autoProxy->position.y > 333)
+	if (input->leftStickDirection.x != 0)
 	{
-		fsmData->autonomousProxy->animator->rotation = 0;
+		autoProxy->acceleration.x = autoProxy->accelerationSpeed * autoProxy->airControlMult * input->leftStickDirection.x;
+	}
+
+	// movement time integration
+	autoProxy->previousPosition = autoProxy->position;
+
+	return;
+
+	if ((autoProxy->velocity.y > 0 && autoProxy->position.y > 333) && (autoProxy->position.x > 170 || autoProxy->position.x < 935))
+	{
+		// land
+		fsmData->autonomousProxy->animator->IncrementOneFrame();
 		fsmData->stateMachine->EnterState(fsmData->availableStates[IDLE_STATE]);
 	}
 }
 
 void SNFSMFallState::Exit(SNFSMData* fsmData)
 {
-	
+
 }
