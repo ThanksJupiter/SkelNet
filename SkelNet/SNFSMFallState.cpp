e0f33563ee080c7c23c53b9f50b12297caa3ff68
ThanksJupiter <35657860+ThanksJupiter@users.noreply.github.com>
2020-01-27 10:39:55 +0100

    added fallstate

---
+++ SkelNet/SNFSMFallState.cpp
@@ -0,0 +1,19 @@
+#include "SNFSMFallState.h"
+#include "SNWorld.h"
+#include "SNAutonomousProxy.h"
+#include "SNAnimator.h"
+
+void SNFSMFallState::Enter(SNFSMData* fsmData)
+{
+	
+}
+
+void SNFSMFallState::Update(float dt, SNFSMData* fsmData)
+{
+	
+}
+
+void SNFSMFallState::Exit(SNFSMData* fsmData)
+{
+	
+}
