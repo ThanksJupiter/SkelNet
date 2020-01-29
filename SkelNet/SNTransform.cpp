cfbf61095a12889af0110ffb2faddd76f70681be
KasperL <45844138+KasperLindm@users.noreply.github.com>
2020-01-29 11:31:09 +0100

    added sets and gets to transform

--- SkelNet/SNTransform.cpp
+++ SkelNet/SNTransform.cpp
@@ -3,6 +3,66 @@
 SNTransform::SNTransform()
 {
 	position = { 0,0 };
-	direction = 0;
+	facingRight = false;
 	scale = { 1,1 };
+}
+
+Vector2 SNTransform::SetPosition(Vector2 inPos)
+{
+	return position = inPos;
+}
+
+Vector2 SNTransform::SetPreviousPosition(Vector2 inPrevPos)
+{
+	return previousPosition = inPrevPos;
+}
+
+Vector2 SNTransform::SetScale(Vector2 inScale)
+{
+	return scale = inScale;
+}
+
+Vector2 SNTransform::SetVelocity(Vector2 inVel)
+{
+	return velocity = inVel;
+}
+
+Vector2 SNTransform::SetAcceleration(Vector2 inAcc)
+{
+	return acceleration = inAcc;
+}
+
+bool SNTransform::SetFacingRight(bool inFacingRight)
+{
+	return facingRight = inFacingRight;
+}
+
+Vector2 SNTransform::GetPosition()
+{
+	return position;
+}
+
+Vector2 SNTransform::GetPreviousPosition()
+{
+	return previousPosition;
+}
+
+Vector2 SNTransform::GetScale()
+{
+	return scale;
+}
+
+Vector2 SNTransform::GetVelocity()
+{
+	return velocity;
+}
+
+Vector2 SNTransform::GetAcceleration()
+{
+	return acceleration;
+}
+
+bool SNTransform::GetFacingRight()
+{
+	return facingRight;
 }
--- SkelNet/SNTransform.h
+++ SkelNet/SNTransform.h
@@ -3,10 +3,27 @@
 
 class SNTransform
 {
+private:
+	Vector2 position;
+	Vector2 previousPosition;
+	Vector2 scale;
+	Vector2 velocity;
+	Vector2 acceleration;
+	bool facingRight;
+
 public:
 	SNTransform();
+	Vector2 GetPosition();
+	Vector2 GetPreviousPosition();
+	Vector2 GetScale();
+	Vector2 GetVelocity();
+	Vector2 GetAcceleration();
+	bool GetFacingRight();
 
-	Vector2 scale;
-	Vector2 position;
-	int direction;
+	Vector2 SetPosition(Vector2 inPos);
+	Vector2 SetPreviousPosition(Vector2 inPos);
+	Vector2 SetScale(Vector2 inPos);
+	Vector2 SetVelocity(Vector2 inPos);
+	Vector2 SetAcceleration(Vector2 inPos);
+	bool SetFacingRight(bool inFacingRight);
 };
