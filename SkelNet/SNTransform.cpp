73a689617ef2d943e162240008a8c23b278d919f
KasperL <45844138+KasperLindm@users.noreply.github.com>
2020-01-29 10:52:49 +0100

    added transform

---
+++ SkelNet/SNTransform.cpp
@@ -0,0 +1,8 @@
+#include "SNTransform.h"
+
+SNTransform::SNTransform()
+{
+	position = { 0,0 };
+	direction = 0;
+	scale = { 1,1 };
+}
---
+++ SkelNet/SNTransform.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Vector.h"
+
+class SNTransform
+{
+public:
+	SNTransform();
+
+	Vector2 scale;
+	Vector2 position;
+	int direction;
+};
