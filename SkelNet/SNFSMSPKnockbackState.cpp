caa0209c73ff1462b134f842ff0d023671df9d13
ThanksJupiter <35657860+ThanksJupiter@users.noreply.github.com>
2020-02-03 14:07:26 +0100

    added particle effect on knockback to simulatsius proximilian

--- SkelNet/SNFSMSPKnockbackState.cpp
+++ SkelNet/SNFSMSPKnockbackState.cpp
@@ -2,6 +2,7 @@
 #include "SNWorld.h"
 #include "SNAnimator.h"
 #include "SNFSMData.h"
+#include "SNParticleSystem.h"
 
 void SNFSMSPKnockbackState::Enter(SNFSMData* fsmData)
 {
@@ -11,7 +12,18 @@ void SNFSMSPKnockbackState::Enter(SNFSMData* fsmData)
 
 void SNFSMSPKnockbackState::Update(float dt, SNFSMData* fsmData)
 {
+	SNInput* input = fsmData->input;
+	SNSimulatedProxy* simProxy = fsmData->simulatedProxy;
 
+	timer += dt;
+
+	if (timer >= dustDelay)
+	{
+		fsmData->world->particleSystem->StartParticleEffect(
+			simProxy->transform.GetPosition(),
+			fsmData->world->dustCloud01Anim, fsmData->world->dustCloud01Anim->duration, simProxy->transform.GetFacingRight());
+		timer = 0.0f;
+	}
 }
 
 void SNFSMSPKnockbackState::Exit(SNFSMData* fsmData)
