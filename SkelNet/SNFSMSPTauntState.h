3c6c14068e79c6c21f690425a9db46ca2836c456
ThanksJupiter <35657860+ThanksJupiter@users.noreply.github.com>
2020-02-05 15:40:13 +0100

    added timer things to SP taunt state

--- SkelNet/SNFSMSPTauntState.h
+++ SkelNet/SNFSMSPTauntState.h
@@ -10,4 +10,9 @@ public:
 	void Enter(SNFSMData* fsmData);
 	void Update(float dt, SNFSMData* fsmData);
 	void Exit(SNFSMData* fsmData);
+
+	float timer = 0.0f;
+	float tauntDuration = 6 * .25;
+	float pufferDelay = 3 * .25;
+	bool hasPufferSoundPlayed = false;
 };
